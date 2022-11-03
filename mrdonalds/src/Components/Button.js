d3417a48a8528e772fb1ac0e07b1a5d64f1ba35f
Elizaveta Zabrodskaya <e_zabrodskaya@wargaming.net>
2022-11-03 11:33:14 +0300

    add Modal and Button

---
+++ mrdonalds/src/Components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonStyled = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+	padding: 20px 35px;
+	min-width: 250px;
+
+	background-color: #299b01;
+	border: none;
+
+	font-family: inherit;
+	font-size: 21px;
+	color: #fff;
+
+	cursor: pointer;
+
+	&:hover {
+		background-color: #35741e;
+	}
+`;
+
+export const Button = () => <ButtonStyled>Добавить</ButtonStyled>;
