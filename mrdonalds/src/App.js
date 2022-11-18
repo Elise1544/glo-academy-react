95e3e5ad8a7d9b410671ac9aa7283927649332ca
Elizaveta Zabrodskaya <e_zabrodskaya@wargaming.net>
2022-11-18 12:01:16 +0300

    add firebase

--- mrdonalds/src/App.js
+++ mrdonalds/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+// import firebase from 'firebase/app';
+// import 'firebase/compat/auth';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+// import {GoogleAuthProvider} from 'firebase/auth';
 import {GlobalStyle} from './Components/Style/GlobalStyle';
 import {NavBar} from './Components/NavBar/Navbar';
 import {Menu} from './Components/Menu/Menu';
@@ -6,16 +11,33 @@ import {ModalItem} from './Components/Modal/ModalItem';
 import {Order} from './Components/Order/Order';
 import {useOpenItem} from './Components/Hooks/useOpenItem';
 import {useOrders} from './Components/Hooks/useOrders';
+import {useAuth} from './Components/Hooks/useAuth';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyA5oYsXo-WMark2eAEFA3jTMdVEiEZqmy0',
+	authDomain: 'mrdonalds-b60b0.firebaseapp.com',
+	databaseURL: 'https://mrdonalds-b60b0-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'mrdonalds-b60b0',
+	storageBucket: 'mrdonalds-b60b0.appspot.com',
+	messagingSenderId: '805376298465',
+	appId: '1:805376298465:web:692aa0776f0db20c1796a0',
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
+	const authFirebase = getAuth(app);
+
+	const auth = useAuth(authFirebase);
+
 	const openItem = useOpenItem();
 	const orders = useOrders();
 
 	return (
 		<>
 			<GlobalStyle />
-			<NavBar />
-			<Order {...orders} {...openItem}></Order>
+			<NavBar {...auth} />
+			<Order {...orders} {...openItem} {...auth}></Order>
 			<Menu {...openItem} />
 			{openItem.openItem && <ModalItem {...openItem} {...orders}></ModalItem>}
 		</>
--- mrdonalds/src/Components/NavBar/Navbar.js
+++ mrdonalds/src/Components/NavBar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import logoImg from '../../image/logo.svg';
-import SignInImg from '../../image/sign.svg';
+import LoginImg from '../../image/sign.svg';
 
 const NavBarStyled = styled.header`
 	position: fixed;
@@ -32,7 +32,7 @@ const ImgLogo = styled.img`
 	width: 50px;
 `;
 
-const SignIn = styled.button`
+const Login = styled.button`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -41,25 +41,51 @@ const SignIn = styled.button`
 	color: white;
 `;
 
-const SignInImage = styled.img`
+const LoginImage = styled.img`
 	width: 32px;
 	height: 32px;
 `;
 
-const SignInText = styled.span`
-	font-size: 16px;
-	margin-top: 3px;
+const User = styled.div`
+	display: flex;
+	align-items: center;
+	text-align: center;
 `;
 
-export const NavBar = () => (
+const LogOut = styled.span`
+	font-size: 20px;
+	font-weight: 700;
+	cursor: pointer;
+	margin-right: 30px;
+`;
+
+const Figure = styled.figure`
+	margin: 0 30px;
+`;
+
+export const NavBar = ({authentication, logIn, logOut}) => (
 	<NavBarStyled>
 		<Logo>
 			<ImgLogo src={logoImg} alt="logo" />
 			<H1>MrDonald's</H1>
 		</Logo>
-		<SignIn>
-			<SignInImage src={SignInImg} alt="account" />
-			<SignInText>войти</SignInText>
-		</SignIn>
+		{authentication ? (
+			<User>
+				<Figure>
+					<LoginImage src={LoginImg} alt={authentication.displayName} />
+					<figcaption>{authentication.displayName}</figcaption>
+				</Figure>
+				<LogOut title="выйти" onClick={logOut}>
+					X
+				</LogOut>
+			</User>
+		) : (
+			<Login onClick={logIn}>
+				<Figure>
+					<LoginImage src={LoginImg} alt="войти" />
+					<figcaption>войти</figcaption>
+				</Figure>
+			</Login>
+		)}
 	</NavBarStyled>
 );
